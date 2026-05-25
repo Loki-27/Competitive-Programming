c80aac2295f4d4fb627c5cf3e11dc56639ad3302
Loki-27 <jangirkrishan28@gmail.com>
2026-05-25 09:06:07 +0530

    Time: 851 ms (5.17%), Space: 18.7 MB (67.71%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+
+    bool BS(vector<vector<int>>& matrix,int t,int Clow ,int Chigh,int Rlow ,int Rhigh){
+        
+        if (Rlow>Rhigh || Clow>Chigh){
+            return false;
+        }
+
+        int Cmid = Clow+ (Chigh-Clow)/2;
+        int Rmid = Rlow + (Rhigh-Rlow)/2;
+
+        if (matrix[Cmid][Rmid]==t) return true; 
+
+        bool f1,f2,f3;
+        if (matrix[Cmid][Rmid]>t){
+            f1 = BS(matrix,t ,Clow ,Cmid-1 ,Rlow ,Rmid-1);
+            
+            f2 = BS(matrix,t ,Cmid ,Chigh ,Rlow, Rmid-1);
+
+            f3 = BS(matrix,t ,Clow ,Cmid-1 ,Rmid, Rhigh);
+            
+        }
+        else{
+            f1 = BS(matrix,t ,Cmid+1 ,Chigh ,Rmid+1 ,Rhigh);
+            
+            f2 = BS(matrix,t ,Clow ,Cmid ,Rmid+1, Rhigh);
+
+            f3 = BS(matrix,t ,Cmid+1 ,Chigh ,Rlow, Rmid);
+        }
+
+        return f1||f2||f3;
+
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int Clow  = 0; 
+        int Chigh = m-1;
+        int Rlow = 0;
+        int Rhigh = n-1;
+        
+        int Cmid = Clow+ (Chigh-Clow)/2;
+
+        int Rmid = Rlow+ (Rhigh-Rlow)/2;
+
+        return BS(matrix,target,Clow,Chigh,Rlow,Rhigh);
+
+    }
+};
