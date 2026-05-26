852b0c2448ba5b142a36bedd39e8b3ca423ccffe
Loki-27 <jangirkrishan28@gmail.com>
2026-05-26 19:40:13 +0530

    Time: 3 ms (56.35%), Space: 16.7 MB (12.4%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int> maper;
+
+        for(int i =0;i<s.size();i++){
+            if (maper.find(s[i]) !=maper.end())
+                maper[s[i]] += 1;
+            else maper[s[i]] = 1;
+        }
+        
+        vector<vector<char>> buckets(s.size()+1);
+
+        for(auto& a:maper){
+            buckets[a.second].push_back(a.first);
+        }
+
+        string ans;
+        for (int i = buckets.size()-1;i>=0;i--){
+            if (!buckets[i].empty()){
+                for(int j = 0; j<buckets[i].size();j++){
+                    for(int freq = 0;freq<i;freq++){
+                        ans.push_back(buckets[i][j]);
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
