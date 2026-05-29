90a2d093c1f8e8631ae30c2e7c0da4726d7cb190
Loki-27 <jangirkrishan28@gmail.com>
2026-05-29 14:38:33 +0530

    Time: 0 ms (100%), Space: 17.6 MB (86.02%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -2,66 +2,22 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
-
-        int firstneg = -1;
-        int lastneg = -1;
-        int segment_start = 0; 
-
-        int ans = INT_MIN;
+        int max_prod = INT_MIN;
         
-        // Use long long to handle potential intermediate overflow safely before clamping to int
-        vector<long long> pfx(n + 1, 1); 
-
+        int prefix = 1;
+        int suffix = 1;
+        
         for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                ans = max(ans, 0); 
-                
-                // Evaluate the segment that just ended before the zero
-                if (i > segment_start) {
-                    long long segment_prod = pfx[i];
-                    if (segment_prod > 0) {
-                        ans = max(ans, (int)segment_prod);
-                    } else if (firstneg != -1) {
-                        // If total product is negative, the max positive product 
-                        // could be found by dropping everything up to the first negative.
-                        if (firstneg + 1 < i) {
-                            ans = max(ans, (int)(segment_prod / pfx[firstneg + 1]));
-                        }
-                    }
-                }
-
-                // Reset trackers for the next segment
-                firstneg = -1;
-                lastneg = -1;
-                segment_start = i + 1;
-                pfx[i + 1] = 1; 
-            } 
-            else {
-                pfx[i + 1] = pfx[i] * nums[i];
-                ans = max(ans, (int)pfx[i + 1]); 
-
-                if (nums[i] < 0) {
-                    if (firstneg == -1) {
-                        firstneg = i;
-                    }
-                    lastneg = i;
-                }
-            }
+            // If previous product became 0, reset it to 1
+            if (prefix == 0) prefix = 1;
+            if (suffix == 0) suffix = 1;
+            
+            prefix *= nums[i];       // Moving left-to-right
+            suffix *= nums[n - 1 - i]; // Moving right-to-left
+            
+            max_prod = max(max_prod, max(prefix, suffix));
         }
-
-        // Handle the final segment if the array didn't end in 0
-        if (n > segment_start) {
-            long long segment_prod = pfx[n];
-            if (segment_prod > 0) {
-                ans = max(ans, (int)segment_prod);
-            } else if (firstneg != -1) {
-                // Drop everything up to the first negative if there are elements remaining
-                if (firstneg + 1 < n) {
-                    ans = max(ans, (int)(segment_prod / pfx[firstneg + 1]));
-                }
-            }
-        }
-
-        return ans;
+        
+        return max_prod;
     }
 };
