852baf1eb87bec4d0e41037a503413646eaeca8e
Loki-27 <jangirkrishan28@gmail.com>
2026-06-01 11:42:38 +0530

    Time: 3 ms (11.9%), Space: 32.2 MB (40.39%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,20 +1,39 @@
 class Solution {
+
 public:
+    bool checker(vector<int>& nums,int tgt,int current_len){
+        int n = nums.size();
+        int sum = accumulate(nums.begin(),nums.begin()+current_len,0);
+        if (n==current_len) return sum>=tgt ? true : false;
+
+        for(int i=0;i+current_len<n;i++){
+            if (sum >= tgt){
+                return true;
+            }
+            else
+                sum = sum -nums[i]+nums[i+current_len];
+        }
+        if (sum >= tgt) return true;
+        return false;
+
+    }
     int minSubArrayLen(int target, vector<int>& nums) {
         int n = nums.size();
-        int ans = INT_MAX;
-        int sum = 0;
-        int j = 0;
-        for(int i = 0;i<n;i++){
-            sum+=nums[i];
+        int ans = 0;
+        int min_len = n;
+        int low = 1;
+        int high = n;
+        while(low<=high){
+            int current_len = low + (high-low)/2;
+            
+            if (checker(nums,target,current_len)){
+                ans = current_len;
+                high = current_len-1;
 
-            while(sum>=target){
-                ans = min(ans,i-j+1);
-                sum-=nums[j];
-                j++;
             }
+            else low = current_len+1;
         }
+        return ans;
 
-        return (ans == INT_MAX) ? 0 : ans;
     }
 };
