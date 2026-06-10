8463562c503a2073462e17e9b1e78e3d96aacee0
Loki-27 <jangirkrishan28@gmail.com>
2026-06-10 23:37:39 +0530

    Time: 0 ms (100%), Space: 25.8 MB (99.62%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,48 +1,76 @@
-class Solution {
-    private:
-    vector<int> PGE(vector<int>&height){
-        stack <int> st;
-        vector <int> next(height.size(),-1);
-        
-        for(int i = 0 ; i < height.size() ; i++){
-            while(!st.empty() && height[i]>height[st.top()]){
-                st.pop();
-            }
-            next[i] = (st.empty()) ? -1 : st.top();
-            st.push(i);
-        }
-        for(auto x: next){
-            cout<<x<< " ";
-        }
-        return next;
-    }
-
-public:
+class Solution{
+    public:
     int trap(vector<int>& height) {
-        vector<int> pge = PGE(height);
-        int n = height.size()-1;
+        int n = height.size();
+        if (n<=2) return 0;
 
-        long long ans = 0;
-        if (n<=1) return 0;
-        // else{
-        //     while(height[n-1]>=height[n]){
-        //         n--;
-        //     }
-        // }
+        int total_water = 0;
+        int l=0,r=n-1,lmax=0,rmax=0;
 
-        int i = n-1;
-        while(n>=0 && i>=0){
-            while((height[i] < height[n] && pge[i]!=-1) && i>=0){
-                i = pge[i];
+        while(l<r){
+            if( height[l] <= height[r]){
+                if(height[l]>=lmax){
+                    lmax = height[l];
+                }
+                else{
+                    total_water += lmax-height[l];
+                }
+                l++;
             }
-            int max_wheight = min(height[i],height[n]);
-
-            for(int x = i+1;x<n;x++){
-                ans += max(0,(max_wheight-height[x]));
+            else{
+                if (height[r]>=rmax){
+                    rmax = height[r];
+                }
+                else{
+                    total_water += rmax-height[r];
+                }
+                r--;
             }
-            n = i;
-            i--;
         }
-        return ans;
+        return total_water;
     }
 };
+
+// class Solution {
+//     private:
+//     vector<int> PGE(vector<int>&height){
+//         stack <int> st;
+//         vector <int> next(height.size(),-1);
+        
+//         for(int i = 0 ; i < height.size() ; i++){
+//             while(!st.empty() && height[i]>height[st.top()]){
+//                 st.pop();
+//             }
+//             next[i] = (st.empty()) ? -1 : st.top();
+//             st.push(i);
+//         }
+//         for(auto x: next){
+//             cout<<x<< " ";
+//         }
+//         return next;
+//     }
+
+// public:
+//     int trap(vector<int>& height) {
+//         vector<int> pge = PGE(height);
+//         int n = height.size()-1;
+
+//         long long ans = 0;
+//         if (n<=1) return 0;
+
+//         int i = n-1;
+//         while(n>=0 && i>=0){
+//             while((height[i] < height[n] && pge[i]!=-1) && i>=0){
+//                 i = pge[i];
+//             }
+//             int max_wheight = min(height[i],height[n]);
+
+//             for(int x = i+1;x<n;x++){
+//                 ans += max(0,(max_wheight-height[x]));
+//             }
+//             n = i;
+//             i--;
+//         }
+//         return ans;
+//     }
+// };
