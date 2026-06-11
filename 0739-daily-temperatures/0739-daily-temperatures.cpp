134435e5f5f5ab8f2f1807f7ed991bdea1ce5528
Loki-27 <jangirkrishan28@gmail.com>
2026-06-11 19:51:35 +0530

    Time: 33 ms (15.96%), Space: 102.7 MB (92.13%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -8,8 +8,7 @@ public:
             while(!st.empty() && temperatures[x] >= temperatures[st.top()]){
                 st.pop();
             }
-            int temp = (st.empty()) ? 0 : temperatures[st.top()];
-            if (temp>temperatures[x]) ans[x] = st.top()-x;
+            if (!st.empty()) ans[x] = st.top()-x;
             else ans[x]=0;
             st.push(x);
         }
