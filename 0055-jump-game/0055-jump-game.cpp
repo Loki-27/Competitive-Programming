b8b7a804b0298fff82d400b6f143c52802a3af28
Loki-27 <jangirkrishan28@gmail.com>
2026-06-24 04:38:28 +0530

    Time: 0 ms (100%), Space: 52.3 MB (83.27%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,39 +1,47 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        if(nums.size()==1) return true;
-
-        if(nums[0]==0) return false;
-
+        int maxidx = 0;
         int n = nums.size();
-        int i = 0;
-        int j = 0;
-        while(j<n){
-            if(j==n-1) return true;
-            if(nums[j]==0){
-                // int k = j;
-                int t = j-1;
-                while(j<n && nums[j]==0){
-                    j++;
-                }
-                while(t>=0){
-                    if(n-1 <= nums[t]+t){
-                        return true;
-                    }
-                    if(j <= nums[t]+t){
-                        if(n-1 <= nums[t]+t){
-                            return true;
-                        }
-                        break;
-                    }
-                    t--;
-                    if(t<0) return false;
-                }
-            }
-            else{
-                j++;
-            }
+        for(int i=0;i<n;i++){
+            if(i>maxidx) return false;
+
+            maxidx = max(maxidx,i+nums[i]);
+            if(maxidx>=n-1) return true;
         }
-        return false;
+        return true;
     }
 };
+
+// if(nums.size()==1) return true;
+
+//         if(nums[0]==0) return false;
+
+//         int n = nums.size();
+//         int j = 0;
+//         while(j<n){
+//             if(j==n-1) return true;
+//             if(nums[j]==0){
+//                 int t = j-1;
+//                 while(j<n && nums[j]==0){
+//                     j++;
+//                 }
+//                 while(t>=0){
+//                     if(n-1 <= nums[t]+t){
+//                         return true;
+//                     }
+//                     if(j <= nums[t]+t){
+//                         if(n-1 <= nums[t]+t){
+//                             return true;
+//                         }
+//                         break;
+//                     }
+//                     t--;
+//                     if(t<0) return false;
+//                 }
+//             }
+//             else{
+//                 j++;
+//             }
+//         }
+//         return false;
