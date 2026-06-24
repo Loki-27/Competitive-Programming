19385da926a17ac836b91df937f8840327c96b72
Loki-27 <jangirkrishan28@gmail.com>
2026-06-24 16:04:15 +0530

    Time: 0 ms (100%), Space: 20.7 MB (36.23%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int l=0,r=0,jumps=0;
+
+        while(r<nums.size()-1){
+            int farthest = 0;
+
+            for(int idx = l;idx <= r;idx++){
+                farthest = max(farthest,idx+nums[idx]);
+            }
+            l = r+1;
+            r=farthest;
+            jumps++;
+        }
+        return jumps;
+    }
+};
