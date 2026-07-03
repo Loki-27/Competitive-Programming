f4519c4d93c183100cd3199488787492dc4478e5
Loki-27 <jangirkrishan28@gmail.com>
2026-07-03 14:19:16 +0530

    Time: 0 ms (100%), Space: 8.2 MB (50.72%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        if(word.size()==1) return true;
+        int c = 0,n=word.size();
+        for(auto& x:word) if(x<='Z') c++;
 
-        bool iscap0 = (word[0]-'A'<=25);
-        bool second = (word[1]-'A' <=25);
-        for(int i =2;i<word.size();i++){
-            if(iscap0 && second) {
-                if (word[i]-'A'>25) return false;
-            }
-            else if(!second){
-                if(word[i]-'A'<=25) return false;
-            }
-            else return false;
-        }
-        if(!iscap0 && second) return false;
-        
-        return true;
+        if(c==0 || c    ==n) return true;
+        if(c==1 && word[0]<='Z') return true;
+        return false;
     }
 };
